a44879c7feee6e2cf233c2e7b9cfca2c1ae3cfa5
Chris Harn <chris.harn@gmail.com>
2023-10-21 11:21:04 -0700

    Simple function implementation that seeds and generates random numbers.

--- FlightEngine/Include/RandomGenerator.h
+++ FlightEngine/Include/RandomGenerator.h
@@ -2,7 +2,11 @@
 #define __RANDOM_GENERATOR__
 
 class RandomGenerator {
-
+public:
+    RandomGenerator();
+    ~RandomGenerator();
+    unsigned int Generate( int offset, int range ); // Returns [offset, offset + range)
+    unsigned char GenerateHeight(); // Returns [0, 255]
 };
 
 #endif
--- FlightEngine/Source/RandomGenerator.cpp
+++ FlightEngine/Source/RandomGenerator.cpp
@@ -1,2 +1,27 @@
 #include "RandomGenerator.h"
 
+#include <iostream> // time
+#include <cstdlib> // srand
+
+RandomGenerator::RandomGenerator() {
+    // Seed generator
+    srand( ( unsigned ) time( nullptr ) );
+}
+
+RandomGenerator::~RandomGenerator() {
+
+}
+
+unsigned int RandomGenerator::Generate( int offset, int range ) {
+    // Generates a number for a range...
+    // offset - starting point for random number
+    // range - the number of values between first 
+    // and last possible random number
+    // i.e. (1, 10) will generate a number between 1 and 10.
+    // (0, 10) will generate a number between 0 and 9.
+    return ( offset + ( rand() % range ) );
+}
+
+unsigned char RandomGenerator::GenerateHeight() {
+    return ( rand() % 256 );
+}
